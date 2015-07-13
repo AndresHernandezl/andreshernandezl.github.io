9b8c0a31592c3df5373354a3715e986deaa7d1f6
Andres Hernandez <tronwares@gmail.com>
2015-07-13 16:29:22 -0500

    Arreglo Menu Y efectos
    Scroll Sky Y fixed al Menu.

--- js/script.js
+++ js/script.js
@@ -17,3 +17,56 @@ var pegarStyky = function(){
 
 }
 $(window).on('scroll',pegarStyky);
+
+
+
+/*Crear ScrollSky*/
+
+$(document).ready(function(){
+    /*
+     * This part causes smooth scrolling using scrollto.js
+     * We target all a tags inside the nav, and apply the scrollto.js to it.
+     */
+    $(".navegador-Item a").click(function(evn){
+        evn.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash);
+    });
+
+    /**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $(".navegador li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("activate");
+            } else {
+                $("a[href='" + theID + "']").removeClass("activate");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("activate")) {
+                var navActiveCurrent = $(".activate").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("activate");
+                $("nav li:last-child a").addClass("activate");
+            }
+        }
+    });
+});
